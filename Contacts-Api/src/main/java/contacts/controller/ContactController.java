a5ae5bb8fea9a9f1b393b2f0c581fef095b89b6e
Xhensila Lakti <43677909+XhensilaLakti@users.noreply.github.com>
2022-11-03 17:00:07 +0100

    Update ContactController.java

--- Contacts-Api/src/main/java/contacts/controller/ContactController.java
+++ Contacts-Api/src/main/java/contacts/controller/ContactController.java
@@ -23,77 +23,69 @@ import com.restfb.types.User;
 import contacts.model.ListaEKontakteve;
 import contacts.service.RegisterNumbers;
 
-
 @RestController
 public class ContactController {
-	
-	
+
 	@Autowired
 	private RegisterNumbers registerNumbers;
-	
-	
+
 	@RequestMapping("/contact")
-	public List<ListaEKontakteve> getContacts() throws FileNotFoundException
-	{
+	public List<ListaEKontakteve> getContacts() throws FileNotFoundException {
 		return registerNumbers.getContact();
 	}
-	
+
 	@RequestMapping("/contact/{name}")
-    public ListaEKontakteve getName(@PathVariable String name ) throws FileNotFoundException
+	public ListaEKontakteve getName(@PathVariable String name) throws FileNotFoundException
 
 	{
 		return registerNumbers.getContact(name);
 	}
-	
-	@RequestMapping(method=RequestMethod.POST,value="/addContact")
-	public void setName(@RequestBody ListaEKontakteve kontakt) 
-	{
+
+	@RequestMapping(method = RequestMethod.POST, value = "/addContact")
+	public void setName(@RequestBody ListaEKontakteve kontakt) {
 		registerNumbers.setContact(kontakt);
-		
+
 	}
-	
-	@RequestMapping(method=RequestMethod.PUT,value="/updateContact/{name}")
-	public void  updateContact(@RequestBody ListaEKontakteve kontakt,@PathVariable String name) throws FileNotFoundException 
-	{
-		registerNumbers.updateContact(kontakt,name);
-		
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/updateContact/{name}")
+	public void updateContact(@RequestBody ListaEKontakteve kontakt, @PathVariable String name)
+			throws FileNotFoundException {
+		registerNumbers.updateContact(kontakt, name);
+
 	}
-	
-	@RequestMapping(method=RequestMethod.DELETE,value="/deleteContact/{name}")
-	public void  deleteContact(@PathVariable String name) 
-	{
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/deleteContact/{name}")
+	public void deleteContact(@PathVariable String name) {
 		registerNumbers.deleteContact(name);
-		
+
 	}
-	@RequestMapping(method=RequestMethod.POST,value="/me/feed")
-	public  List<String> fbFeed() {
-		String stringAccess="EAACMZBrHD01YBAM5QgJBZA6X4kqkj3o1VRQKZBKwTGXUpWOQRTXzLpEFlZCZBBY4XYTNJ9WjlfL1wBnTOvTeEfs9BRh5wMX79srbMLeLaA4dVagDGLJ1FQnZCUE5ZCn0MBwsAEGXZA3uudyxbjzB1xmec1ZA4b8dpzUNhhu9OhNWbDgZDZD";
-		
-		FacebookClient fbClient = new DefaultFacebookClient(stringAccess,Version.VERSION_3_1);
-		User me =fbClient.fetchObject("me", User.class);
-		AccessToken exAccessToken =fbClient.obtainExtendedAccessToken("155008352113494","ec53c601d70c654bb93ab389fbb75e20");
-		Connection <Post> results=fbClient.fetchConnection("me/feed", Post.class);
-		
-		List<String> posts=new ArrayList<String>();
-		for(List<Post> page:results)
-		
-		{for(Post aPost:page)
-		{ 
-		 posts.add(aPost.getMessage()+" "+"fb.com/"+aPost.getId());
-		
-			
-		}
-		
-		
-			
+
+	@RequestMapping(method = RequestMethod.POST, value = "/me/feed")
+	public List<String> fbFeed() {
+		String stringAccess = "EAACMZBrHD01YBAM5QgJBZA6X4kqkj3o1VRQKZBKwTGXUpWOQRTXzLpEFlZCZBBY4XYTNJ9WjlfL1wBnTOvTeEfs9BRh5wMX79srbMLeLaA4dVagDGLJ1FQnZCUE5ZCn0MBwsAEGXZA3uudyxbjzB1xmec1ZA4b8dpzUNhhu9OhNWbDgZDZD";
+
+		FacebookClient fbClient = new DefaultFacebookClient(stringAccess, Version.VERSION_3_1);
+		User me = fbClient.fetchObject("me", User.class);
+		AccessToken exAccessToken = fbClient.obtainExtendedAccessToken("155008352113494",
+				"ec53c601d70c654bb93ab389fbb75e20");
+		Connection<Post> results = fbClient.fetchConnection("me/feed", Post.class);
+
+		List<String> posts = new ArrayList<String>();
+		for (List<Post> page : results)
+
+		{
+			for (Post aPost : page) {
+				posts.add(aPost.getMessage() + " " + "fb.com/" + aPost.getId());
+
+			}
+
 		}
 		return posts;
-		
-	
 
-}
-	@RequestMapping(method=RequestMethod.POST,value="/xheni")
-	public String xheni(){
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/xheni")
+	public String xheni() {
 		return "xheni";
 	}
 }
